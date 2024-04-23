7242fc384db7cf00178ccf1a0936550fff6d7318
mozaicdb <mozaic138@gmail.com>
2024-04-23 03:15:47 +0000

    just enjoying the ride but little stressfull no sleep for me

---
+++ 0x00-ES6_basic/12-main.js
@@ -0,0 +1,13 @@
+bob@dylan:~$ cat 12-main.js
+import createEmployeesObject from './11-createEmployeesObject.js';
+import createReportObject from './12-createReportObject.js';
+
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};      
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
+
