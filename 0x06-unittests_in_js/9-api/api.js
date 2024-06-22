8e9dfdbedd8050d726c7100bc1eedb408581197e
mozaicdb <mozaic138@gmail.com>
2024-06-22 08:52:25 +0000

    work on it 😘💕

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,20 +1,17 @@
 const express = require('express');
 
 const app = express();
-const PORT = 7865;
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = req.params.id;
-
-  res.send(`Payment methods for cart ${id}`);
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
